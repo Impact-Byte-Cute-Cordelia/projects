dac2a9a418cad9d04eec52034bb1c4587dc42eb4
thoriq nur faizal <thoriqnfaizal@gmail.com>
2020-04-02 11:08:38 +0700

    adding materi npm

---
+++ npm/index.js
@@ -0,0 +1,12 @@
+let a = "b";
+console.log(a);
+
+// how to install node
+// how to install npm
+// npm init
+// edit package.json script
+// uninstall npm package
+// using yarn
+
+// npm install & uninstall
+// yarn add & remove
