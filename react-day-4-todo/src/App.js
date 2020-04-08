09a86e283abfd074a28fec2bc2dfb0603c25358e
thoriq nur faizal <thoriqnfaizal@gmail.com>
2020-04-08 10:10:59 +0700

    Adding add todo

---
+++ react-day-4-todo/src/App.js
@@ -0,0 +1,45 @@
+import React, { useState } from "react";
+import Todo from "./components/Todo";
+
+const App = () => {
+  const [todos, setTodos] = useState([
+    { text: "Learn react" },
+    { text: "Learn at impactbyte" },
+    { text: "love programming" },
+  ]);
+
+  const [value, setValue] = useState("");
+  console.log("value", value);
+
+  // handle change input
+  const handleChange = (event) => {
+    setValue(event.target.value);
+  };
+
+  // handle submit form
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    console.log("isi value", value);
+    if (!value) return alert("todo belum diisi");
+    const newTodos = [...todos, { text: value }];
+    console.log("newTodos", newTodos);
+    setTodos(newTodos);
+    setValue("");
+  };
+  return (
+    <div>
+      <h1>React Todo function</h1>
+      {/* untuk form input */}
+      <form onSubmit={handleSubmit}>
+        <input type="text" value={value} onChange={handleChange} />
+        <input type="submit" />
+      </form>
+
+      {todos.map((todo, index) => (
+        <Todo key={index} index={index} todo={todo} />
+      ))}
+    </div>
+  );
+};
+
+export default App;
