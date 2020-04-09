a7f1db9d1854748f18a9e112ee95cf8e080df659
auzanassdq <auzanassidqi@gmail.com>
2020-04-09 11:47:38 +0700

    add react-axios

---
+++ react-day-5-axios/src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+import TodoList from './components/TodoList'
+import DigimonList from './components/DigimonList'
+
+function App() {
+  return (
+    <div>
+      {/* <TodoList /> */}
+      <DigimonList/>
+    </div>
+  );
+}
+
+export default App;
