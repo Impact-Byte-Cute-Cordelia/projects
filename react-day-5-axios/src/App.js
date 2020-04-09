0d25f131fcd2e78d12590c9fa5998b8b907349fc
thoriq nur faizal <thoriqnfaizal@gmail.com>
2020-04-09 12:13:47 +0700

    Adding fetch api unsplash

--- react-day-5-axios/src/App.js
+++ react-day-5-axios/src/App.js
@@ -1,15 +1,17 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
-import TodoList from './components/TodoList'
-import DigimonList from './components/DigimonList'
+import TodoList from "./components/TodoList";
+import DigimonList from "./components/DigimonList";
+import UnsplashList from "./components/UnsplashList";
 
 function App() {
   return (
     <div>
       {/* <TodoList /> */}
-      <DigimonList/>
+      {/* <DigimonList /> */}
+      <UnsplashList />
     </div>
   );
 }
