93257eb249fee9d9868800022e14c9f824df1501
thoriq nur faizal <thoriqnfaizal@gmail.com>
2020-04-20 13:43:06 +0700

    Adding logger and devtools

--- review-redux/src/store.js
+++ review-redux/src/store.js
@@ -1,4 +1,9 @@
-import { createStore } from "redux";
+import { createStore, applyMiddleware } from "redux";
 import rootReducers from "./Reducers";
+import logger from "redux-logger";
+import { composeWithDevTools } from "redux-devtools-extension";
 
-export default createStore(rootReducers);
+export default createStore(
+  rootReducers,
+  composeWithDevTools(applyMiddleware(logger))
+);
